97599de74bba044194b0e1563bf18e17ed102075
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-30 20:59:46 +0530

    Update token expiry time and add test for sending sales hourly data

--- tests/TangentApiClientTest.php
+++ tests/TangentApiClientTest.php
@@ -13,7 +13,7 @@ it('gets a valid token', function () {
     Http::fake([
         'example.com/*' => Http::response([
             'access_token' => 'valid_token',
-            'expires_in' => 3600,
+            'expires_in' => 30*60,
         ]),
     ]);
 
@@ -33,4 +33,30 @@ it('gets a valid token', function () {
     expect($token)->toBe('valid_token');
     expect(session('token'))->toBe('valid_token');
     expect(session('token_expiry'))->toBeGreaterThan(time());
+});
+
+it('can send sales hourly data', function () {
+    Http::fake([
+        'example.com/*' => Http::response([
+            'access_token' => 'valid_token',
+            'expires_in' => 30*60,
+        ]),
+    ]);
+
+    $client = new TangentApiClient([
+        'base_uri' => 'http://example.com',
+        'grant_type' => 'client_credentials',
+        'username' => 'test_user',
+        'password' => 'test_password',
+    ]);
+
+    $response = $client->sendSalesHourly([
+        // todo: add test data
+    ]);
+
+    Http::assertSent(function ($request) {
+        return $request->url() == 'http://example.com/SalesHourly';
+    });
+
+    expect($response->ok())->toBeTrue();
 });
