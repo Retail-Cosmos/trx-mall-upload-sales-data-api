5d678d529043b1c151be1ffa39021555cabf045e
Punyapal Shah <mrpunyapal@gmail.com>
2023-12-30 19:06:49 +0530

    Add API configuration for sending sales data

--- config/trx-mall-upload-sales-data-api.php
+++ config/trx-mall-upload-sales-data-api.php
@@ -1,6 +1,10 @@
 <?php
 
-// config for RetailCosmos/TrxMallUploadSalesDataApi
 return [
-
+    'api' => [
+        'base_uri' => env('TANGENT_BASE_URI'),
+        'grant_type' => env('TANGENT_GRANT_TYPE'),
+        'username' => env('TANGENT_USERNAME'),
+        'password' => env('TANGENT_PASSWORD'),
+    ],
 ];
