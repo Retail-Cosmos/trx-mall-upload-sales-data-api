627e5a1c0147c4e74ed6022959bd4532e4fe8c3a
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-01 20:49:52 +0530

    Add support for logging with Laravel Command

--- src/Support/Log.php
+++ src/Support/Log.php
@@ -2,18 +2,25 @@
 
 namespace RetailCosmos\TrxMallUploadSalesDataApi\Support;
 
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log as LogFacade;
 
 class Log
 {
-    public static function info(string $message, array $context = []): void
+    public static function info(string $message, array $context = [], ?Command $command = null): void
     {
+        if ($command) {
+            $command->info($message);
+        }
         LogFacade::channel(config('trx-mall-upload-sales-data-api.log.channel'))
             ->info($message, $context);
     }
 
-    public static function error(string $message, array $context = []): void
+    public static function error(string $message, array $context = [], ?Command $command = null): void
     {
+        if ($command) {
+            $command->error($message);
+        }
         LogFacade::channel(config('trx-mall-upload-sales-data-api.log.channel'))
             ->error($message, $context);
     }
