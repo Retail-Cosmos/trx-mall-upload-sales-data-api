1121a2d544ef88e3d87c9bc0432a2b1df5b6f069
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-01 20:49:57 +0530

    Refactor logging in LogTest.php

--- tests/LogTest.php
+++ tests/LogTest.php
@@ -1,7 +1,8 @@
 <?php
 
-use RetailCosmos\TrxMallUploadSalesDataApi\Support\Log;
+use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log as LogFacade;
+use RetailCosmos\TrxMallUploadSalesDataApi\Support\Log;
 
 it('can log info message', function () {
     $message = 'This is an info message';
@@ -17,7 +18,7 @@ it('can log info message', function () {
 
     config(['trx-mall-upload-sales-data-api.log.channel' => 'stack']);
 
-    Log::info($message, $context);
+    Log::info(message: $message, context: $context);
 });
 
 it('can log error message', function () {
@@ -34,5 +35,49 @@ it('can log error message', function () {
 
     config(['trx-mall-upload-sales-data-api.log.channel' => 'stack']);
 
-    Log::error($message, $context);
+    Log::error(message: $message, context: $context);
+});
+
+it('can log info message with command', function () {
+    $message = 'This is an info message';
+    $context = ['key' => 'value'];
+
+    $command = mock(Command::class);
+    $command->shouldReceive('info')
+        ->with($message)
+        ->once();
+
+    LogFacade::shouldReceive('channel')
+        ->with('stack')
+        ->andReturnSelf();
+
+    LogFacade::shouldReceive('info')
+        ->with($message, $context)
+        ->once();
+
+    config(['trx-mall-upload-sales-data-api.log.channel' => 'stack']);
+
+    Log::info($message, $context, $command);
+});
+
+it('can log error message with command', function () {
+    $message = 'This is an error message';
+    $context = ['key' => 'value'];
+
+    $command = mock(Command::class);
+    $command->shouldReceive('error')
+        ->with($message)
+        ->once();
+
+    LogFacade::shouldReceive('channel')
+        ->with('stack')
+        ->andReturnSelf();
+
+    LogFacade::shouldReceive('error')
+        ->with($message, $context)
+        ->once();
+
+    config(['trx-mall-upload-sales-data-api.log.channel' => 'stack']);
+
+    Log::error($message, $context, $command);
 });
