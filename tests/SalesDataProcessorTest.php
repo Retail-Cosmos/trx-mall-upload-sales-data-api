8d1165f7d6b223925638e9530a63bd76cb4ed4b7
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-04 20:05:49 +0530

    Refactor sales data transformation and validation tests

--- tests/SalesDataProcessorTest.php
+++ tests/SalesDataProcessorTest.php
@@ -1,38 +1,70 @@
 <?php
 
+use RetailCosmos\TrxMallUploadSalesDataApi\Enums\PaymentType;
 use RetailCosmos\TrxMallUploadSalesDataApi\Services\SalesDataProcessor;
 
-todo('transforms the sales data', function () {
-    $sales = [
-        // todo: sample sales data here
+it('transforms the sales data', function () {
+    $sales = collect([
+        [
+            'happened_at' => '2023-02-01 00:00:00',
+            'gto' => 191.54,
+            'gst' => 1.55,
+            'discount' => 0,
+            'servicecharge' => 5.00,
+            'noofpax' => 0,
+            PaymentType::CASH() => 8.97,
+            PaymentType::TNG() => 0,
+            PaymentType::VISA() => 76.78,
+            PaymentType::MASTERCARD() => 0,
+            PaymentType::AMEX() => 47.80,
+            PaymentType::VOUCHER() => 0,
+            PaymentType::OTHERS() => 57.99,
+        ], [
+            'happened_at' => '2023-02-01 00:00:00',
+            'gto' => 391.54,
+            'gst' => 12.65,
+            'discount' => 10,
+            'servicecharge' => 0.00,
+            'noofpax' => 0,
+            PaymentType::CASH() => 18.97,
+            PaymentType::TNG() => 0,
+            PaymentType::VISA() => 176.78,
+            PaymentType::MASTERCARD() => 0,
+            PaymentType::AMEX() => 47.80,
+            PaymentType::VOUCHER() => 0,
+            PaymentType::OTHERS() => 0,
+        ],
+    ]);
+    $storeData = [
+        'machineid' => '123',
+        'gstregistered' => 'Y',
     ];
 
     $processor = new SalesDataProcessor();
-    $processor->process($sales);
+    $result = $processor->process($sales, $storeData);
 
-    // todo: assertions to test the transformation of sales data
-});
+    expect(count($result))->toBe(24);
 
-todo('validates the sales data', function () {
-    $sales = [
-        // todo: sample sales data here
-    ];
+    $sale = collect($result)->where('sale.hour', 0)->first();
 
-    $processor = new SalesDataProcessor();
-
-    expect(function () use ($processor, $sales) {
-        $processor->process($sales);
-    })->not->toThrow();
-});
-
-todo('throws an exception for invalid sales data', function () {
-    $sales = [
-        // todo: sample invalid sales data here
-    ];
-
-    $processor = new SalesDataProcessor();
-
-    expect(function () use ($processor, $sales) {
-        $processor->process($sales);
-    })->toThrow(\Exception::class);
+    expect($sale['sale'])->toBe([
+        'machineid' => '123',
+        'batchid' => 1,
+        'date' => '20230201',
+        'hour' => 0,
+        'receiptcount' => 2,
+        'gto' => 583.08,
+        'gst' => 14.20,
+        'discount' => 10.00,
+        'servicecharge' => 5.00,
+        'noofpax' => 0,
+        PaymentType::CASH() => 27.94,
+        PaymentType::TNG() => 0.00,
+        PaymentType::VISA() => 253.56,
+        PaymentType::MASTERCARD() => 0.00,
+        PaymentType::AMEX() => 95.60,
+        PaymentType::VOUCHER() => 0.00,
+        PaymentType::OTHERS() => 57.99,
+        'gstregistered' => 'Y',
+    ]);
 });
