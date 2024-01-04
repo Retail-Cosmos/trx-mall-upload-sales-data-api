41cff6635f1667dccb7477da2fb9e8d9213a94ad
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-04 21:50:26 +0530

    Add StoreDataProcessor class for processing store data

---
+++ src/Services/StoreDataProcessor.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RetailCosmos\TrxMallUploadSalesDataApi\Services;
+
+class StoreDataProcessor
+{
+    public function process(array $storeData): array
+    {
+        $this->validate($storeData);
+
+        $this->prepare($storeData);
+
+        return $storeData;
+    }
+
+    /**
+     * @param  array<string,mixed>  $storeData
+     *
+     * @throws \Exception
+     */
+    private function validate(array $storeData): void
+    {
+        $validator = validator($storeData, [
+            '*.machine_id' => 'required|distinct',
+            '*.store_identifier' => 'required|distinct',
+            '*.gst_registered' => 'required|boolean',
+        ]);
+
+        if ($validator->fails()) {
+            throw new \Exception($validator->errors()->first());
+        }
+
+    }
+
+    /**
+     * @param  array<string,mixed>  $storeData
+     */
+    private function prepare(array &$storeData): void
+    {
+        foreach ($storeData as &$store) {
+            $store['gst_registered'] = $store['gst_registered'] ? 'Y' : 'N';
+        }
+    }
+}
