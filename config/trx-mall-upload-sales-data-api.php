ba8a08fa90350762a8f2c0b933fa824a9a790b89
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-06 14:29:42 +0530

    Add mail recipient for notifications

--- config/trx-mall-upload-sales-data-api.php
+++ config/trx-mall-upload-sales-data-api.php
@@ -25,4 +25,15 @@ return [
     'log' => [
         'channel' => env('TRX_MALL_LOG_CHANNEL', 'stack'),
     ],
+
+    /**
+     * recipient for the notification
+     * mail.name and mail.email are used for the recipient
+     */
+    'notifications' => [
+        'mail' =>[
+            'name' => env('TRX_MALL_API_MAIL_NAME'),
+            'email' => env('TRX_MALL_API_MAIL_EMAIL'),
+        ]
+    ],
 ];
