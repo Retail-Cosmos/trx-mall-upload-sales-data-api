fcd0aaa9f186a889da53bcee4fb2545909bcabbf
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-10 19:31:13 +0530

    Update comment

--- tests/SendSalesCommandTest.php
+++ tests/SendSalesCommandTest.php
@@ -128,7 +128,7 @@ describe('success cases without notification', function () {
 
     afterEach(function () {
         Notification::assertNothingSent();
-        Http::assertSentCount(3); // 1 for fetching token, 2 for sending sales
+        Http::assertSentCount(3); // 1 for fetching token, 2 for sending sales for 2 stores
     });
 });
 
@@ -162,7 +162,7 @@ describe('success cases with notification', function () {
                 && $notification->name === $this->mailConfig['name'];
             }
         );
-        Http::assertSentCount(3); // 1 for fetching token, 2 for sending sales
+        Http::assertSentCount(3); // 1 for fetching token, 2 for sending sales for 2 stores
     });
 });
 
