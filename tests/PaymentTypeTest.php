fbdc25134c43d65f9a539c3eb771280293ea60ea
Punyapal Shah <mrpunyapal@gmail.com>
2024-01-11 20:23:17 +0530

    Fix exception message in PaymentTypeTest

--- tests/PaymentTypeTest.php
+++ tests/PaymentTypeTest.php
@@ -10,4 +10,4 @@ it('returns enum value by calling static method', function () {
 
 it('throws exception when calling non-existing static method', function () {
     PaymentType::NON_EXISTING_METHOD();
-})->throws(Exception::class, "No static method or enum constant 'NON_EXISTING_METHOD' in class RetailCosmos\TrxMallUploadSalesDataApi\Enums\PaymentType");
+})->throws(Exception::class);
